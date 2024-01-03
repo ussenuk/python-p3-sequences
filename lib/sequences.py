71a43abd3439455cd69a9113d06e974b1711a6c5
ussenuk <ukimanuka@gmail.com>
2024-01-03 18:16:13 +0300

    worked and understood the fibonacci series

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        fib = []
+    elif length == 1:
+        fib = [0]
+    elif length == 2:
+        fib = [0, 1]
+    else:
+        fib =[0, 1]
+        for i in range (2, length):
+            fib.append(fib[i-1] + fib[i-2])
+    print(fib)
+    return fib
+    
